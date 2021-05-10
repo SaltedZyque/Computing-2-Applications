6d893b7e50c8978cfd89fa544e354acf95e43d97
Zhao Qi / Zyque <zhao-qi.so20@imperial.ac.uk>
2021-05-10 16:17:31 +0100

    Update c1-exam.js

--- Week 2 - Javascript/web-app/c1-exam.js
+++ Week 2 - Javascript/web-app/c1-exam.js
@@ -15,9 +15,7 @@ const Exam = Object.create(null);
 //      an input list of [1,2,3,4,5,6,7,8]
 //      returns [1,4,7]
 
-Exam.every_third = function (array) {
-    return array.filter((ignore, k) => k % 3 === 0);
-};
+trying to create a conflict here
 
 // Exam.every_third = function (input_array) {
 
